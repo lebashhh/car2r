412236261404b7d56fae693223e8752d08d1ade3
323 <323@323-3>
2025-04-22 09:44:08 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Car.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Car
+    {
+        private string mark;
+        private string number;
+        private int mileage;
+
+        // Свойства для работы с полями
+        public string Brand
+        {
+            get { return mark; }
+            set { mark = value; }
+        }
+
+        public string LicensePlate
+        {
+            get { return number; }
+            set { number = value; }
+        }
+
+        public int Mileage
+        {
+            get { return mileage; }
+            set { mileage = value; }
+        }
+
+        // Конструкторы
+        public Car()
+        {
+            mark = "Не указано";
+            number = "Не указан";
+            mileage = 0;
+        }
+
+        public Car(string brand, string licensePlate, int mileage)
+        {
+            this.mark = brand;
+            this.number = licensePlate;
+            this.mileage = mileage;
+        }
+
+        // Метод для вывода данных об автомобиле
+        public void Show()
+        {
+            Console.WriteLine($"Марка: {mark}");
+            Console.WriteLine($"Гос. номер: {number}");
+            Console.WriteLine($"Пробег: {mileage} км");
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+
+            // 3. Задание значений свойств
+            myCar.Brand = "Жигули";
+            myCar.LicensePlate = "Т144ТО";
+            myCar.Mileage = 10350;
+
+            // 4. Вывод значений свойств
+            Console.WriteLine("Информация об автомобиле:");
+            myCar.Show();
+
+            // 5. Расчет среднего годового пробега
+            int age = 5;
+            double averageAnnualMileage = (double)myCar.Mileage / age;
+            Console.WriteLine($"\nСредний годовой пробег за {age} лет: {averageAnnualMileage} км/год");
+            Console.ReadKey();
+        }
+    }
+}
